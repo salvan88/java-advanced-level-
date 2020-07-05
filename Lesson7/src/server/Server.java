4f3e610fb9b32665d902ba80ab9bb65244c2ee7e
salvan88 <gerzen777@gmail.com>
2020-07-05 11:42:41 +0300

    Практическая работа 7

---
+++ Lesson7/src/server/Server.java
@@ -0,0 +1,78 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Server {
+    private List<ClientHandler> clients;
+    private AuthService authService;
+
+    public AuthService getAuthService() {
+        return authService;
+    }
+
+    public Server() {
+        clients = new Vector<>();
+        authService = new SimpleAuthService();
+
+        ServerSocket server = null;
+        Socket socket;
+
+        final int PORT = 8189;
+
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Сервер запущен!");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент подключился");
+                System.out.println("socket.getRemoteSocketAddress(): " + socket.getRemoteSocketAddress());
+                System.out.println("socket.getLocalSocketAddress() " + socket.getLocalSocketAddress());
+                new ClientHandler(this, socket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    void broadcastMsg(String msg) {
+        for (ClientHandler client : clients) {
+            client.sendMsg(msg);
+        }
+    }
+
+    void broadcastPrivateMsg(String msg, String nick1, String nick2) {
+        for (ClientHandler client : clients) {
+            if (client.getNick().equals(nick1))
+                client.sendMsg("You whisper to " + nick2 + ": " + msg);
+
+            if (client.getNick().equals(nick2))
+                client.sendMsg(nick1 + " whisper: " + msg);
+
+        }
+    }
+
+    public void subscribe(ClientHandler clientHandler) {
+        clients.add(clientHandler);
+    }
+
+    public void unsubscribe(ClientHandler clientHandler) {
+        clients.remove(clientHandler);
+    }
+
+}
